20a62b481e96b4529cf5ec36e829305f2357e56d
rinyaresu <kaiquelinhares17@outlook.com>
2022-01-24 09:18:47 -0300

    testing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Coin.create!(
+  description: 'Bitcoin',
+  acronym: 'BTC',
+  url_image: 'https://tradingplatforms.com/pt/wp-content/uploads/sites/27/2021/04/Bitcoin-Logo-1024x576.png'
+)
+
+Coin.create!(
+  description: 'Shiba Inu Coin',
+  acronym: 'SHIBA',
+  url_image: 'https://s2.coinmarketcap.com/static/img/coins/200x200/5994.png'
+)
