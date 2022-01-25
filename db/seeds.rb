ed8b42254d4c6558d9a0358b804a2e5709a3a445
rinyaresu <kaiquelinhares17@outlook.com>
2022-01-25 10:01:18 -0300

    refactoring seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,25 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Coin.create!(
-  description: 'Bitcoin',
-  acronym: 'BTC',
-  url_image: 'https://tradingplatforms.com/pt/wp-content/uploads/sites/27/2021/04/Bitcoin-Logo-1024x576.png'
-)
 
-Coin.create!(
-  description: 'Shiba Inu Coin',
-  acronym: 'SHIBA',
-  url_image: 'https://s2.coinmarketcap.com/static/img/coins/200x200/5994.png'
-)
+coins = [
+          {
+            description: 'Bitcoin',
+            acronym: 'BTC',
+            url_image: 'https://assets.chinatechnews.com/wp-content/uploads/bitcoin-logo.jpg'
+          },
+          {
+            description: 'Ethereum',
+            acronym: 'ETH',
+            url_image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZCRfwkqpPvFb3QmmwGONG2i6PsgnqZ3L7dRzCNlaSTB1-ruu5'
+          },
+          {
+            description: 'Shiba Inu Coin',
+            acronym: 'SHIBA',
+            url_image: 'https://s2.coinmarketcap.com/static/img/coins/200x200/5994.png'
+          }
+        ]
 
-Coin.create!(
-  description: 'Ethereum',
-  acronym: 'ETH',
-  url_image: 'https://d33wubrfki0l68.cloudfront.net/fcd4ecd90386aeb50a235ddc4f0063cfbb8a7b66/4295e/static/bfc04ac72981166c740b189463e1f74c/40129/eth-diamond-black-white.jpg'
-)
+coins.each do |coin|
+  Coin.find_or_create_by!(coin)
+end
